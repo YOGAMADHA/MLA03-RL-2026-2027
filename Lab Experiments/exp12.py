7ba27cf4952185572b652fc12f12b687a767a6a9
YOGAMADHA <yogamadha126@gmail.com>
2026-07-31 21:37:01 +0530

    Update exp12.py

--- Lab Experiments/exp12.py
+++ Lab Experiments/exp12.py
@@ -1,1 +1,12 @@
+import random
 
+actions = ["Left", "Right", "Up", "Down"]
+scores = {a: 0 for a in actions}
+
+for _ in range(100):
+    action = random.choice(actions)
+    reward = 1 if action == "Right" else -1
+    scores[action] += reward
+
+print("Learned Policy:", scores)
+print("Best Action:", max(scores, key=scores.get))
