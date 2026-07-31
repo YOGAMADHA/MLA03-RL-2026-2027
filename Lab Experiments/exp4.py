475309eda6ddbda575b4b4c79c9447bb333eb517
YOGAMADHA <yogamadha126@gmail.com>
2026-07-31 21:33:51 +0530

    Add minimum travel cost calculation in exp4.py
    Implement cost calculation for travel using dynamic programming.

--- Lab Experiments/exp4.py
+++ Lab Experiments/exp4.py
@@ -1,1 +1,10 @@
+V = {"A": 0, "B": 0, "C": 0, "Goal": 0}
 
+cost = {"A": 4, "B": 3, "C": 1}
+
+for _ in range(10):
+    V["C"] = cost["C"] + V["Goal"]
+    V["B"] = cost["B"] + V["C"]
+    V["A"] = cost["A"] + V["B"]
+
+print("Minimum Travel Cost:", V["A"])
