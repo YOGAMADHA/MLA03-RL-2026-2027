4f082a97f09935f499fee5dc7d1719b59c1a8dd0
YOGAMADHA <yogamadha126@gmail.com>
2026-07-31 21:37:45 +0530

    Update exp14.py

--- Lab Experiments/exp14.py
+++ Lab Experiments/exp14.py
@@ -1,1 +1,12 @@
+import random
 
+actions = ["Up", "Down", "Stop"]
+value = {a: 0 for a in actions}
+
+for _ in range(100):
+    action = random.choice(actions)
+    reward = random.choice([-1, 1, 5])
+    value[action] += reward
+
+print("Elevator Policy:", value)
+print("Best Action:", max(value, key=value.get))
