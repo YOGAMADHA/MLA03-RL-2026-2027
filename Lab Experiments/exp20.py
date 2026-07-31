f51ad254272252d8512006ef360255f2c7ea8699
YOGAMADHA <yogamadha126@gmail.com>
2026-07-31 21:40:18 +0530

    Update exp20.py

--- Lab Experiments/exp20.py
+++ Lab Experiments/exp20.py
@@ -1,1 +1,17 @@
+import random
 
+observations = ["Clear", "Obstacle", "Victim"]
+
+for _ in range(5):
+    observation = random.choice(observations)
+
+    if observation == "Victim":
+        action = "Rescue"
+    elif observation == "Obstacle":
+        action = "Turn"
+    else:
+        action = "Move"
+
+    print("Observation:", observation, "Action:", action)
+
+print("POMDP Search and Rescue Completed.")
