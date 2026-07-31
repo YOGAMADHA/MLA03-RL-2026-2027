52d19b1551c6bf6653108c63e446d5ea497ce848
YOGAMADHA <yogamadha126@gmail.com>
2026-07-31 21:35:57 +0530

    Implement Q-learning algorithm in exp9.py

--- Lab Experiments/exp9.py
+++ Lab Experiments/exp9.py
@@ -1,1 +1,11 @@
+import random
 
+Q = [0, 0]
+
+for _ in range(100):
+    action = random.randint(0, 1)
+    reward = random.choice([-1, 10])
+
+    Q[action] += 0.1 * (reward - Q[action])
+
+print("TD/SARSA/Q-Learning Values:", Q)
