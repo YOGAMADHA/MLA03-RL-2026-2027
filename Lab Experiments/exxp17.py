7497d7038dff92123114e495f28af1ed44e405c0
YOGAMADHA <yogamadha126@gmail.com>
2026-07-31 21:38:46 +0530

    Update exxp17.py

--- Lab Experiments/exxp17.py
+++ Lab Experiments/exxp17.py
@@ -1,1 +1,9 @@
+tasks = {
+    "Cleaning": ["Move", "Pick", "Clean"],
+    "Cooking": ["Move", "Pick", "Cook"]
+}
 
+for task, actions in tasks.items():
+    print(task, ":", " -> ".join(actions))
+
+print("Household tasks completed.")
